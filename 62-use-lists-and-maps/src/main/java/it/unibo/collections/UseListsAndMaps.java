8ca84ffe96391cb2e6961e0af16b35ba7f0aa098
MonkeSam <samuele.t.030204@gmail.com>
2024-11-04 17:20:45 +0100

    push

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,12 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -12,28 +17,45 @@ public final class UseListsAndMaps {
     private UseListsAndMaps() {
     }
 
+    public void getTime() {
+
+    }
+
     /**
      * @param s
-     *            unused
+     *          unused
      */
     public static void main(final String... s) {
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> ali = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            ali.add(1000 + i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        List<Integer> lli = new LinkedList<>();
+        lli = ali;
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+
+        int tmp;
+        tmp = ali.get(ali.size() - 1);
+        ali.set(ali.size() - 1, ali.get(0));
+        ali.set(0, tmp);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        System.out.println(ali);
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,6 +63,29 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        int elems = 100000;
+        /*
+         * Prepare a variable for measuring time
+         */
+        long time = System.nanoTime();
+        /*
+         * Run the benchmark
+         */
+        for (int i = 1; i <= elems; i++) {
+            ali.addFirst(i);
+        }
+        /*
+         * Compute the time and print result
+         */
+        time = System.nanoTime() - time;
+        System.out.println(// NOPMD
+                "Converting "
+                        + elems
+                        + " ints to String and inserting them in a Set took "
+                        + time
+                        + "ns ("
+                        + TimeUnit.NANOSECONDS.toMillis(time)
+                        + "ms)");
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
